0fceb271ac29c793f6ba363e1f3ece1d8a97992a
Ivan Berdichevsky <berdichevskyivan@gmail.com>
2024-07-19 21:38:30 +0000

    Added change to limiter

--- backend/node/server.js
+++ backend/node/server.js
@@ -46,8 +46,8 @@ app.use('/uploads', express.static(join(__dirname, 'uploads')));
 
 // Configure rate limiting
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 200, // Limit each IP to 100 requests per windowMs
+  windowMs: 2 * 60 * 1000, // 15 minutes
+  max: 400, // Limit each IP to 400 requests per windowMs
   message: "Too many requests from this IP, please try again later"
 });
 
